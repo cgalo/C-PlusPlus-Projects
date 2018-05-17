2954a27295b51e5634ba6b17222c3728b7d79ffd
cgalo <cgalo13@gmail.com>
2018-05-17 14:19:43 -0600

    Update ParkingGarage.cpp

--- ParkingGarage.cpp
+++ ParkingGarage.cpp
@@ -7,32 +7,32 @@
  *        input the program will read the hours of usage for each of the 5 cars. The program will print the results in the 
  *        form of a table in a neat format
  */
- 
- #include <iostream>
- #include <string>
- #include <tgmath.h>
- using namespace std;
- 
-int main(){
-  double tot_hrs; hrs; tot_charge;
-  int round_hrs;
-  for (int i = 1; i < 6; i++){
-    cout << "Enter the hours parked for car " << i << ": ";
-    cin  >> hours;
-    if (hrs < 2 ){
-      tot_charge += 5;
-    }
-    else if (hrs > 9) {
-      tot_charge += 12;
-    }
-    else if (hrs >= 2){
-      round_hrs   = ceil(hrs);
-      tot_charge += 5 + (round_hrs-2.0);
-    }
-    tot_hrs += hrs;
-  }
-  cout << "Total Hours "   << tot_hrs    << endl;
-  cout << "Total Charge $" << tot_charge << endl;
- 
-  return 0;
+
+#include <iostream>
+#include <string>
+#include <tgmath.h>
+using namespace std;
+
+int main (){
+    double tot_hrs, hrs, tot_charge;
+    int round_hrs;
+    for (int i = 1; i < 6; i++){                                //For loop for to calculate the 5 cars
+        cout << "Enter the hours parked for car " << i << ": "; //User input
+        cin  >> hrs;
+        if (hrs < 2){                                           //If less than 2hrs. charge only 5$
+            tot_charge += 5;
+        }
+        else if (hrs > 9){                                     //If more than 9hrs. charge only max ($12.00)
+            tot_charge += 12; 
+        }
+        else if (hrs >= 2){                                    //If more equal or more than 2 hours then charge $5 + $1 per hour
+            round_hrs   = ceil(hrs);
+            tot_charge += 5 + (round_hrs-2.0);
+        }
+        tot_hrs += hrs;                                         //Adds the hours per loop to total hours of the whole for-loop
+    }     
+    cout << "Total Hours "   << tot_hrs    << endl;             //Output
+    cout << "Total Charge $" << tot_charge << endl;
+    
+    return 0;
 }
